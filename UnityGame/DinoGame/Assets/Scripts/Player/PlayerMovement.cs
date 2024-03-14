3f212c19d7cba96bb6380d8a8a321678c3030c4f
Victor Sanchez <vickoss9004@gmail.com>
2024-03-14 15:03:03 -0600

    Movimiento de jugador
    me falto el salto pero queda en 5min, tambien agregue armas y una escena para testear, luego hago las armas funcionales

---
+++ UnityGame/DinoGame/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController controller;
+    public float speed;
+    public float gravity=-9.81f;
+    Vector3 velocity;
+
+    public Transform groundCheck;
+    public float groundDistance = .4f;
+    public LayerMask groundMask;
+    bool isGrounded;
+
+    void Update()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2;
+        }
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        controller.Move(move * speed * Time.deltaTime);
+
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+    }
+}
