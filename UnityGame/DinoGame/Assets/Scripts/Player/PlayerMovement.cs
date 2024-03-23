dd154e232b8467bd846c9d8cc73758a69d76b42b
Victor Sanchez <vickoss9004@gmail.com>
2024-03-23 14:10:41 -0600

    Detalles

--- UnityGame/DinoGame/Assets/Scripts/Player/PlayerMovement.cs
+++ UnityGame/DinoGame/Assets/Scripts/Player/PlayerMovement.cs
@@ -27,9 +27,7 @@ public class PlayerMovement : MonoBehaviour
         float z = Input.GetAxis("Vertical");
 
         Vector3 move = transform.right * x + transform.forward * z;
-
         controller.Move(move * speed * Time.deltaTime);
-
         velocity.y += gravity * Time.deltaTime;
         controller.Move(velocity * Time.deltaTime);
     }
