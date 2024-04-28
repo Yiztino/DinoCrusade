7d5bec67b537bd91b76d2bf0b60a4b07b1522617
Victor Sanchez <vickoss9004@gmail.com>
2024-04-27 19:37:11 -0600

    Sistema de puntos y kills funcionando
    Falta otorgar los puntos de las rondas.

--- UnityGame/DinoGame/Assets/Scripts/Dinos/DinosaurHealth.cs
+++ UnityGame/DinoGame/Assets/Scripts/Dinos/DinosaurHealth.cs
@@ -1,7 +1,5 @@
-using System.Collections;
-using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
-using Unity.AI;
 
 public class DinosaurHealth : MonoBehaviour
 {
@@ -13,18 +11,37 @@ public class DinosaurHealth : MonoBehaviour
 
     private FollowPlayer followPlayer;
 
+    public int Points;
+    public TextMeshProUGUI pointsText;
+    public TextMeshProUGUI pointsTextRound;
+    public TextMeshProUGUI killsText;
+
+    private bool isDead = false;
+    private int kills;
+
 
     private void Start()
     {
-        vidaActual = vidaMaxima; 
+        vidaActual = vidaMaxima;
         myAnim = GetComponent<Animator>();
         body = GetComponent<MeshCollider>();
         followPlayer = GetComponent<FollowPlayer>();
+        pointsText = GameObject.Find("FPS_UI/Points").GetComponent<TextMeshProUGUI>();
+        pointsTextRound = GameObject.Find("BetweenRounds/BG/Points").GetComponent<TextMeshProUGUI>();
+        killsText = GameObject.Find("BetweenRounds/BG/Kills").GetComponent<TextMeshProUGUI>();
+        if (pointsText == null)
+        {
+            Debug.LogError("No se encontró el TextMeshProUGUI para los puntos.");
+        }
+        if (pointsTextRound == null)
+        {
+            Debug.LogError("No se encontró el TextMeshProUGUI para el resumen de puntos.");
+        }
     }
 
-    public void TakeDamage(int cantidadDanio)
+    public void TakeDamage(int dmgAmout)
     {
-        vidaActual -= cantidadDanio; 
+        vidaActual -= dmgAmout;
 
         if (vidaActual <= 0)
         {
@@ -34,6 +51,9 @@ public class DinosaurHealth : MonoBehaviour
 
     private void Death()
     {
+        if (isDead) return;
+
+        isDead = true;
         followPlayer.StopMoving();
         body.enabled = false;
         myAnim.SetBool("isDead", true);
@@ -48,8 +68,52 @@ public class DinosaurHealth : MonoBehaviour
                 mouthCollider.enabled = false;
             }
         }
-        
+
+        AddKill();
+        AddPoints(Points);
         Destroy(gameObject, 5);
+    }
+
+    public void AddPoints(int pointsToAdd)
+    {
+        Points = PlayerPrefs.GetInt("PlayerPoints", 0);
+        Points += pointsToAdd;
+        pointsText.text = "Points: " + Points.ToString();
+        pointsTextRound.text = "Points: " + Points.ToString();
+        PlayerPrefs.SetInt("PlayerPoints", Points);
+        PlayerPrefs.Save();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Y))
+        {
+            ResetPoints();
+            ResetKills();
+        }
+    }
+
+    public void AddKill()
+    {
+        kills = PlayerPrefs.GetInt("Kills", 0);
+        kills ++;
+        killsText.text = "Kills: " + kills.ToString();
+        PlayerPrefs.SetInt("Kills", kills) ;
+        PlayerPrefs.Save();
+    }
+
+    public void ResetPoints()
+    {
+        PlayerPrefs.DeleteKey("PlayerPoints");
+        Points = 0;
+        pointsText.text = "Points: " + Points.ToString();
+    }
+
+    public void ResetKills()
+    {
+        PlayerPrefs.DeleteKey("Kills");
+        kills = 0;
+        killsText.text = "Kills: " + Points.ToString();
     }
 
 }
